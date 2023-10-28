58eae8f2bb36903d821d1452fb10e85b179f4e11
Mike Peace <mikepeace981@gmail.com>
2023-10-27 23:13:58 -0700

    scond commit

--- components/Socials.js
+++ components/Socials.js
@@ -2,7 +2,7 @@
 import Link from "next/link";
 
 // icons
-import {RiYoutubeLine,
+import {
 RiInstagramLine,
 RiFacebookLine,
 RiTwitterLine,
@@ -16,25 +16,25 @@ RiPinterestLine,
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/mike-mutuku-0a243a1bb/'} className='hover:text-accent transition-all duration-300'>
         <RiInstagramLine/>
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/mike-mutuku-0a243a1bb/'} className='hover:text-accent transition-all duration-300'>
         <RiFacebookLine/>
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/mike-mutuku-0a243a1bb/'} className='hover:text-accent transition-all duration-300'>
         <RiTwitterLine/>
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/mike-mutuku-0a243a1bb/'} className='hover:text-accent transition-all duration-300'>
         <RiGithubLine/>
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/mike-mutuku-0a243a1bb/'} className='hover:text-accent transition-all duration-300'>
         <RiDribbleLine/>
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/mike-mutuku-0a243a1bb/'} className='hover:text-accent transition-all duration-300'>
         <RiBehanceLine/>
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/mike-mutuku-0a243a1bb/'} className='hover:text-accent transition-all duration-300'>
         <RiPinterestLine/>
       </Link>
     </div>
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,273 @@
+// import React, { useState } from "react";
+
+// // icons
+// import {
+//   FaHtml5,
+//   FaCss3,
+//   FaJs,
+//   FaReact,
+//   FaWordpress,
+//   FaFigma,
+// } from "react-icons/fa";
+
+// import {
+//   SiNextdotjs,
+//   SiFramer,
+//   SiAdobexd,
+//   SiAdobephotoshop,
+// } from "react-icons/si";
+
+// //  data
+// const aboutData = [
+//   {
+//     title: "skills",
+//     info: [
+//       {
+//         title: 'Web Development',
+//         icons: [
+//           <FaHtml5 />,
+//           <FaCss3 />,
+//           <FaJs />,
+//           <FaReact />,
+//           <SiNextdotjs />,
+//           <SiFramer />,
+//           <FaWordpress />,
+//         ],
+//       },
+//       {
+//         title: "UI/UX Design",
+//         icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+//       },
+//     ],
+//   },
+//   {
+//     title: "Certificates",
+//     info: [
+//       {
+//         title: "Digital journalism",
+//         stage: "Reuters 2023",
+//       },
+//       {
+//         title: "Digital marketing",
+//         stage: "Google digital garage 2022",
+//       },
+//       {
+//         title: "Introduction to HTML,CSS, Javascript",
+//         stage: "Sololearn 2023",
+//       },
+//     ],
+//   },
+//   {
+//     title: 'experience',
+//     info: [
+//       {
+//         title: 'UX/UI Designer - XYZ Company',
+//         stage: '2012 - 2023',
+//       },
+//       {
+//         title: 'Web Developer - ABC Agency',
+//         stage: '2010 - 2012',
+//       },
+//       {
+//         title: 'Intern - DEF Corporation',
+//         stage: '2008 - 2010',
+//       },
+//     ],
+//   },
+//   {
+//     title: "credentials",
+//     info: [
+//       {
+//         title: "Software Engineering Diploma - Nairobits",
+//         stage: "2023",
+//       },
+//       {
+//         title: "Certified Digital literature - Nairobits",
+//         stage: "2022",
+//       },
+//     ],
+//   },
+// ];
+
+// // components
+// import Avatar from "../../components/Avatar.js";
+// import Circles from "../../components/Circles.js";
+
+// // framer motion
+// import { motion } from "framer-motion";
+// import { fadeIn } from "../../variants.js";
+
+// // counter
+// import CountUp from "react-countup/";
+
+// const About = () => {
+//   const [index, setIndex] = useState(0);
+//   console.log(index);
+//   return (
+//     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+//       <Circles />
+
+//       {/* avatar img */}
+//       <motion.div
+//         variants={fadeIn("right", 0.2)}
+//         initial="hidden"
+//         animate="show"
+//         exit="hidden"
+//         className="hidden xl:flex absolute bottom-0 -left-[370px]"
+//       >
+//         <Avatar />
+//       </motion.div>
+//       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+
+//         {/* text */}
+
+//         <div className="flex-1 flex flex-col justify-center">
+//           <motion.h2
+//             variants={fadeIn("right", 0.2)}
+//             initial="hidden"
+//             animate="show"
+//             exit="hidden"
+//             className="h2"
+//           >
+//             Captivating <span className="text-accent">stories</span> birth
+//             magnificent designs.
+//           </motion.h2>
+
+//           <motion.p
+//             variants={fadeIn("right", 0.4)}
+//             initial="hidden"
+//             animate="show"
+//             exit="hidden"
+//             className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+//           >
+//             A year ago I began galavanting the tech world mainly through digital
+//             marketing and graphic design, then, I honed myself with knowledge in
+//             Software Engineering. Since then I have done probono work for
+//             startups, consulted for newbie entrepreneurs, and collaborated on
+//             digital products for businesses and consumer use. Wanna be the next
+//             one?
+//           </motion.p>
+
+//           {/* counters */}
+
+//           <motion.div
+//             variants={fadeIn("right", 0.6)}
+//             initial="hidden"
+//             animate="show"
+//             exit="hidden"
+//             className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+//           >
+//             <div className="flex flex-1 xl:gap-x-6">
+//               {/* experience */}
+
+//               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+//                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//                   <CountUp start={0.1} end={0.6} duration={8} /> +
+//                 </div>
+//                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//                   Years of experience
+//                 </div>
+//               </div>
+
+//               {/* Clients */}
+
+//               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+//                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//                   <CountUp start={0} end={10} duration={5} /> +
+//                 </div>
+//                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//                   Satisfied clients
+//                 </div>
+//               </div>
+
+//               {/* Projects */}
+//               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+//                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//                   <CountUp start={0} end={35} duration={5} /> +
+//                 </div>
+//                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//                   Finished projects
+//                 </div>
+//               </div>
+
+//               {/* Certificates and honors */}
+//               <div className="relative flex-1">
+//                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//                   <CountUp start={0} end={8} duration={5} /> +
+//                 </div>
+//                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//                   Certificates and honors
+//                 </div>
+//               </div>
+//             </div>
+//           </motion.div>
+//         </div>
+
+//         {/* info */}
+
+//         <motion.div
+//           variants={fadeIn("left", 0.4)}
+//           initial="hidden"
+//           animate="show"
+//           exit="hidden"
+//           className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
+//         >
+//           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+//             {aboutData.map((item, itemIndex) => {
+//               return (
+//                 <div
+//                   key={itemIndex}
+//                   className={`${
+//                     index === itemIndex &&
+//                     "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+//                   } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+//                   onClick={() => setIndex(itemIndex)}
+//                 >
+//                   {item.title}
+//                 </div>
+//               );
+//             })}
+//           </div>
+
+//           {/* Disciplines I do */}
+          
+//           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+//             {aboutData[index].info.map((item, itemIndex) => {
+//               return (
+//                 <div
+//                   key={itemIndex}
+//                   className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+//                 >
+
+//                   {/* title */}
+//                   <div className="font-light mb-2 md:mb-8">{item.title}</div>
+//                   <div className="hidden md:flex">.</div>
+//                   <div>{item.stage}</div>
+//                   <div className="flex gap-x-4">
+//                     {/* icons */}
+
+//                     {item.icons?.map((icon, itemIndex) => {
+//                       return(
+//                         <div
+//                         key={itemIndex}
+//                          className="text-2xl text-white">{icon}
+//                         </div>
+//                       );
+//                     })}
+//                   </div>
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         </motion.div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default About;
+
+
 import React, { useState } from "react";
 
 // icons
@@ -17,7 +287,7 @@ import {
   SiAdobephotoshop,
 } from "react-icons/si";
 
-//  data
+// data
 const aboutData = [
   {
     title: "skills",
@@ -25,18 +295,18 @@ const aboutData = [
       {
         title: 'Web Development',
         icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          <FaHtml5 key={0} />,
+          <FaCss3 key={1} />,
+          <FaJs key={2} />,
+          <FaReact key={3} />,
+          <SiNextdotjs key={4} />,
+          <SiFramer key={5} />,
+          <FaWordpress key={6} />,
         ],
       },
       {
         title: "UI/UX Design",
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        icons: [<FaFigma key={0} />, <SiAdobexd key={1} />, <SiAdobephotoshop key={2} />],
       },
     ],
   },
@@ -120,7 +390,6 @@ const About = () => {
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
 
         {/* text */}
-
         <div className="flex-1 flex flex-col justify-center">
           <motion.h2
             variants={fadeIn("right", 0.2)}
@@ -149,7 +418,6 @@ const About = () => {
           </motion.p>
 
           {/* counters */}
-
           <motion.div
             variants={fadeIn("right", 0.6)}
             initial="hidden"
@@ -159,7 +427,6 @@ const About = () => {
           >
             <div className="flex flex-1 xl:gap-x-6">
               {/* experience */}
-
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0.1} end={0.6} duration={8} /> +
@@ -170,7 +437,6 @@ const About = () => {
               </div>
 
               {/* Clients */}
-
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={10} duration={5} /> +
@@ -204,7 +470,6 @@ const About = () => {
         </div>
 
         {/* info */}
-
         <motion.div
           variants={fadeIn("left", 0.4)}
           initial="hidden"
@@ -234,20 +499,20 @@ const About = () => {
             {aboutData[index].info.map((item, itemIndex) => {
               return (
                 <div
-                  key={itemIndex}
+                  key={itemIndex} // Added key prop here
                   className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
                 >
-
                   {/* title */}
                   <div className="font-light mb-2 md:mb-8">{item.title}</div>
                   <div className="hidden md:flex">.</div>
                   <div>{item.stage}</div>
                   <div className="flex gap-x-4">
                     {/* icons */}
-
-                    {item.icons?.map((icon, itemIndex) => {
-                      return(
-                        <div className="text-2xl text-white">{icon}</div>
+                    {item.icons?.map((icon, iconIndex) => {
+                      return (
+                        <div key={iconIndex} className="text-2xl text-white">
+                          {icon}
+                        </div>
                       );
                     })}
                   </div>
